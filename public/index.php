2981328c2e073789597ee9684ef34af63ba994c8
Archie Makuwa <archie@aatsol.co.za>
2015-09-14 21:55:52 +0200

    Updated gitignore

---
+++ public/index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Archie Makuwa | Labs Indexp</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
+    <!--<link rel="stylesheet" href="css/estilos.css"> -->
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+    <style>
+        .center{
+            margin: 0px 20px;
+        }
+        .medium{
+            max-width: 900px;
+            margin: 0px auto;
+        }
+    </style>
+</head>
+
+<body>
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Archie Makuwa | Labs</a>
+        </div>
+    </div>
+</nav>
+<div class="panel panel-default medium">
+    <div class="panel-heading">
+        <h3 class="panel-title">Project / Labs</h3>
+    </div>
+    <div class="panel-body">
+        <div class="center">
+            <table class="table">
+                <thead>
+                    <th>#</th>
+                    <th>Project</th>
+                    <th>Description</th>
+                    <th>Status</th>
+                    <th>Url</th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1.</td>
+                        <td>WordPress</td>
+                        <td>WordPress contribution and plugin staging</td>
+                        <td>ongoing build</td>
+                        <td><a href="#" target="_blank">View</a></td>
+                    </tr>
+                    <tr>
+                    <tr>
+                        <td>2.</td>
+                        <td>2016</td>
+                        <td>WordPress Twenty 16 theme</td>
+                        <td>ongoing</td>
+                        <td><a href="#" target="_blank">View</a></td>
+                    </tr>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+</body>
+</html>
+
